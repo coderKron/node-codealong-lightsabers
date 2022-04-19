7df7e9fabfc3ca0e2e8e1106b16e86dcea2872cd
Ruben Poelen <ruben.poelen@hotmail.com>
2022-04-19 15:58:47 +0200

    Initial commit: basic express app structure

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/about", (req, res, next) => {
+  console.log("A request on the ABOUT page was received...");
+
+  res.sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/contact", (req, res, next) => {
+  res.sendFile(__dirname + "/views/contact.html");
+});
+
+app.listen(3001, () => {
+  console.log("server listening to requests on port 3000");
+});
